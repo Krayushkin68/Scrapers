f8e7c67adff9d4751edaba92dae739ce67fa86a6
Krayushkin68 <krayuskhinml97@gmail.com>
2021-09-26 21:37:00 +0300

    bar chart auth complete

--- barchart_scraper/main.py
+++ barchart_scraper/main.py
@@ -1,4 +1,4 @@
-# import requests
+import requests
 import requests_html
 import fake_useragent
 from bs4 import BeautifulSoup as bs
@@ -10,22 +10,30 @@ login_url = 'https://www.barchart.com/login'
 login = 'angarragnar777@rambler.ru'
 password = 'Gfhjkmangarragnar777'
 
-payload = {'email': login,
-           'password': password}
-
+# rs = requests.Session()
 rs = requests_html.HTMLSession()
+login_page = rs.get(login_url, headers=headers)
+print('Login page code ', login_page.status_code)
+
+login_html = bs(login_page.content, 'html.parser')
+token = login_html.select('input[name="_token"]')[0].get('value')
+print('Token: ', token)
+
+payload = {'_token': token,
+           'email': login,
+           'password': password,
+           'remember': 'on'}
+
 logging = rs.post(login_url, headers=headers, data=payload)
+print('Logging code ', logging.status_code)
+# rs.get(login_url, headers=headers)
 
-main_page = 'https://www.barchart.com/'
-page = rs.get(main_page, headers=headers)
-page.html.render(sleep=1, keep_page=True)
+page_url = 'https://www.barchart.com/options/most-active/stocks?orderBy=optionsTotalVolume&orderDir=desc'
+page = rs.get(page_url, headers=headers)
+print('Main page code ', page.status_code)
 
-with open('test.html', 'wb') as f:
-    f.write(page.content)
+# page.html.render(sleep=1, keep_page=True)
 
 html = bs(page.content, 'html.parser')
-tag1 = html.select('span.bc-glyph-user')
-tag2 = html.select('a.bc-user-block__button')
-print(tag1)
-print(tag2)
+print(html)
 
